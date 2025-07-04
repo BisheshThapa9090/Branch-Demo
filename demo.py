e72ed03bb0ddd7e51b0f61bbe562714c6d5dae3b
BisheshThapa9090 <tbishesh4@gmail.com>
2025-07-04 09:42:55 +0545

    Initial commit with demo.py and images

---
+++ demo.py
@@ -0,0 +1,5 @@
+# This is a demo for learning Python basics
+# LOL just practicing like a classroom task 😄
+print("Hello! This is a Python demo.")
+print("We are doing classwork lol 😂\n")
+print("\nClasswork finished. This was just a demo lol 😎")
